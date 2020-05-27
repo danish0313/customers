0a6656df50ecca0c7ca0774e51839ccc8d666804
danish0313 <danishoo.dk@gmail.com>
2020-05-27 22:18:19 +0200

    Updates

---
+++ precache-manifest.18eb723672432040a8980bb5b851d634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835dfe91a5cb9c645415ccd939eb672d",
+    "url": "/customers/index.html"
+  },
+  {
+    "revision": "3eace31556370395de1c",
+    "url": "/customers/static/css/main.2557b320.chunk.css"
+  },
+  {
+    "revision": "6f2d49f882ac544673eb",
+    "url": "/customers/static/js/2.a3880bd3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/customers/static/js/2.a3880bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eace31556370395de1c",
+    "url": "/customers/static/js/main.60038c84.chunk.js"
+  },
+  {
+    "revision": "a84b035384c568e3b672",
+    "url": "/customers/static/js/runtime-main.ea59afd8.js"
+  }
+]);
